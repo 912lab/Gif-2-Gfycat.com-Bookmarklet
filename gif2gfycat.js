e01022250cc0889f723f78202b77417d643a4ed9
912lab <adrian.spiegel@me.com>
2017-03-27 22:50:12 +0200

    Create gif2gfycat.js

---
+++ gif2gfycat.js
@@ -0,0 +1,1 @@
+javascript:(function gifToGfycat() { var gifLink = window.location.host + window.location.pathname; var newUrl = "http://gfycat.com/fetch/" + gifLink; window.open(newUrl, "gfycat"); })()
